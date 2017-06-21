f96cdb33bad6f4372333779497c91a0844970732
selfresonator <selfresonator@gmail.com>
2017-06-20 22:05:52 -0700

    Navbar position fix

--- src/components/FrontPage/PageContent/TwoLine/index.js
+++ src/components/FrontPage/PageContent/TwoLine/index.js
@@ -5,7 +5,7 @@ import './style.css';
 const TwoLine = () => (
   <section className="three">
     <div className="info top">
-      <p className="box-title"><span className="bold">Left</span> side two-line heading.</p>
+      <p className="box-title"><span className="bold">Left</span> side two-line <br/>heading.</p>
       <p className="box-info">Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor.</p>
     </div>
     
