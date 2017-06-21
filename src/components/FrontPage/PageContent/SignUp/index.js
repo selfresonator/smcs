2e7c45f9210e303b12effdecf9cb479ba85c32ee
selfresonator <selfresonator@gmail.com>
2017-06-20 21:56:58 -0700

    Mobile is fixed, now to tackle the footer

--- src/components/FrontPage/PageContent/SignUp/index.js
+++ src/components/FrontPage/PageContent/SignUp/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './style.scss';
+import './style.css';
 
 const SignUp = () => (
   <section className="sign-up">
--- src/components/FrontPage/PageContent/TwoLine/index.js
+++ src/components/FrontPage/PageContent/TwoLine/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './style.scss';
+import './style.css';
 
 const TwoLine = () => (
   <section className="three">
