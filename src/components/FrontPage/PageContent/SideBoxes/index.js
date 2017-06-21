cdb33afcd823d903aa1896d5897584a9333f4951
selfresonator <selfresonator@gmail.com>
2017-06-21 01:23:02 -0700

    Working on rendering svg images

--- src/components/FrontPage/PageContent/SideBoxes/index.js
+++ src/components/FrontPage/PageContent/SideBoxes/index.js
@@ -12,7 +12,7 @@ const SideBoxes = () => (
       </div>
       
       <div className="page-info right">
-        <img src="icon-lock.svg" alt="" className="base lock"/>
+        <object data="icon-lock.svg" type="image/svg+xml" className="base lock"></object>
         <p className="box-title"><span className="bold">Right</span> side box</p>
         <p className="box-info">Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula.</p>
       </div>
