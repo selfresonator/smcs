ba2a472b3782b37e6e7d9e641882809e01deacac
selfresonator <selfresonator@gmail.com>
2017-06-20 19:40:18 -0700

    Few fixes for last image

--- src/components/FrontPage/PageContent/index.js
+++ src/components/FrontPage/PageContent/index.js
@@ -41,17 +41,19 @@ class PageContent extends Component {
           
           {/*Two-Line Heading*/}
           <section className="three">
-            <div className="info">
+            <div className="info top">
               <p className="box-title"><span className="bold">Left</span> side two-line heading.</p>
               <p className="box-info">Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor.</p>
             </div>
             
-            <div className="img">
+            <div className="img top">
               <img alt="" className="win"/>
             </div>
           </section>
           
-          <section></section>
+          <section>
+            
+          </section>
           
         </div>
         
